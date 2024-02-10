de54ffa02f6373a88af086c43a97bdc756513b27
liwenka1 <2020583117@qq.com>
2024-02-10 13:49:37 +0800

    fix: 修改avatar

--- packages/components/avatar/src/avatar.tsx
+++ packages/components/avatar/src/avatar.tsx
@@ -15,16 +15,16 @@ Avatar.displayName = AvatarPrimitive.Root.displayName
 const AvatarImage = React.forwardRef<
   React.ElementRef<typeof AvatarPrimitive.Image>,
   React.ComponentPropsWithoutRef<typeof AvatarPrimitive.Image>
->(() => {
-  return <AvatarPrimitive.Image />
+>(({ className, ...props }, ref) => {
+  return <AvatarPrimitive.Image className={cn("", className)} ref={ref} {...props} />
 })
 AvatarImage.displayName = AvatarPrimitive.Image.displayName
 
 const AvatarFallback = React.forwardRef<
   React.ElementRef<typeof AvatarPrimitive.Fallback>,
   React.ComponentPropsWithoutRef<typeof AvatarPrimitive.Fallback>
->(() => {
-  return <AvatarPrimitive.Fallback />
+>(({ className, ...props }, ref) => {
+  return <AvatarPrimitive.Fallback className={cn("", className)} ref={ref} {...props} />
 })
 AvatarFallback.displayName = AvatarPrimitive.Fallback.displayName
 

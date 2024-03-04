e1bea347083e6d931b58ebe60e493ac6be43fb37
liwenka1 <2020583117@qq.com>
2024-03-04 14:58:34 +0800

    fix: 添加动画库 修改readme

--- packages/components/accordion/src/accordion-item.tsx
+++ packages/components/accordion/src/accordion-item.tsx
@@ -1,6 +1,7 @@
 import * as React from "react"
 
 import { ChevronDownIcon } from "@radix-ui/react-icons"
+import { motion } from "framer-motion"
 
 import {
   AccordionContentVariants,
@@ -27,15 +28,26 @@ const AccordionItem = React.forwardRef<HTMLDivElement, AccordionItemProps>((prop
     ...otherProps
   } = useAccordionItem(props)
 
+  const variants = {
+    open: { opacity: 1, y: 0 },
+    closed: { opacity: 0, y: "100%" }
+  }
+
+  const [isOpen, setIsOpen] = React.useState(false)
+
   return (
     <Component className={cn(AccordionItemVariants({ className }))} ref={ref} value={value} {...otherProps}>
-      <HeaderComponent className={cn(AccordionHeaderVariants({}))}>
+      <HeaderComponent className={cn(AccordionHeaderVariants({}))} onClick={() => setIsOpen((isOpen) => !isOpen)}>
         <TriggerComponent className={cn(AccordionTriggerVariants({}))}>
           {label}
           <ChevronDownIcon className="transition-transform delay-200" />
         </TriggerComponent>
       </HeaderComponent>
-      <ContentComponent className={cn(AccordionContentVariants({}))}>{children}</ContentComponent>
+      <ContentComponent className={cn(AccordionContentVariants({}))}>
+        <motion.div animate={isOpen ? "open" : "closed"} variants={variants}>
+          {children}
+        </motion.div>
+      </ContentComponent>
     </Component>
   )
 })
